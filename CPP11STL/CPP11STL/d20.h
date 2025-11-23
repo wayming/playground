2ac546db062c09feced4c27f8dac3b0a7f5ee11f
way <wayming.z@gmail.com>
2025-11-23 21:58:12 +1000

    d20

--- CPP11STL/CPP11STL/d20.h
+++ CPP11STL/CPP11STL/d20.h
@@ -1,11 +1,4 @@
-#include <future>
-#include <functional>
 #include <iostream>
-
-// thread_pool.hpp
-#ifndef THREAD_POOL_HPP
-#define THREAD_POOL_HPP
-
 #include <vector>
 #include <queue>
 #include <thread>
@@ -17,59 +10,106 @@
 
 class ThreadPool {
 public:
-    explicit ThreadPool(std::size_t threads = std::thread::hardware_concurrency())
-        : stop(false) {
-        for (std::size_t i = 0; i < threads; ++i)
-            workers_.emplace_back([this] {
-                for (;;) {
-                    std::function<void()> task;
-                    {
-                        std::unique_lock<std::mutex> lock(queue_mutex_);
-                        condition_.wait(lock, [this] { return stop || !tasks_.empty(); });
-                        if (stop && tasks_.empty()) return;
-                        task = std::move(tasks_.front());
-                        tasks_.pop();
-                    }
+    ThreadPool(size_t n = std::thread::hardware_concurrency()) {
+        for (size_t i = 0; i < n; ++i) {
+            threads.emplace_back([this](){
+                while (true) {
+                    std::unique_lock<std::mutex> lock(mtx);
+                    cv.wait(lock, [this](){ return stop || !taskQueue.empty();});
+                    if (stop && taskQueue.empty()) return;
+                    auto task = std::move(taskQueue.front());
+                    taskQueue.pop();
                     task();
                 }
             });
+        }
     }
 
-    // 提交任务到池子，返回 std::future<ReturnType>
-    template<class F, class... Args>
-    auto enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type> {
-        using ReturnType = typename std::result_of<F(Args...)>::type;
-        auto task = std::make_shared< std::packaged_task<ReturnType()> >(
+    template <typename F, typename... Args>
+    std::future<typename std::result_of<F(Args...)>::type> enqueue(F&& f, Args&&... args) {
+        using RET = decltype(std::forward<F>(f)(std::forward<Args>(args)...));
+        auto task = std::make_shared<std::packaged_task<RET()>>(
             std::bind(std::forward<F>(f), std::forward<Args>(args)...)
         );
-        std::future<ReturnType> res = task->get_future();
+        auto fu = task->get_future();
         {
-            std::unique_lock<std::mutex> lock(queue_mutex_);
-            if (stop) throw std::runtime_error("enqueue on stopped ThreadPool");
-            tasks_.emplace([task]() { (*task)(); });
+            std::scoped_lock<std::mutex> lock(mtx);
+            if (stop) throw std::runtime_error("enqueue stopped.");
+            taskQueue.emplace([task]() mutable { (*task)(); });
         }
-        condition_.notify_one();
-        return res;
+        cv.notify_one();
+        return fu;
     }
 
     ~ThreadPool() {
-        {
-            std::unique_lock<std::mutex> lock(queue_mutex_);
-            stop = true;
-        }
-        condition_.notify_all();
-        for (std::thread& worker : workers_) worker.join();
+        stop = true;
+        cv.notify_all();
+        for (auto& t : threads) t.join();
     }
-
 private:
-    std::vector<std::thread> workers_;
-    std::queue<std::function<void()>> tasks_;
-    std::mutex queue_mutex_;
-    std::condition_variable condition_;
-    bool stop;
+    std::vector<std::thread> threads;
+    std::queue<std::function<void()>> taskQueue;
+    std::mutex mtx;
+    std::condition_variable cv;
+    std::atomic<bool> stop = false;
 };
 
-#endif // THREAD_POOL_HPP
+
+// class ThreadPool {
+// public:
+//     explicit ThreadPool(std::size_t threads = std::thread::hardware_concurrency())
+//         : stop(false) {
+//         for (std::size_t i = 0; i < threads; ++i)
+//             workers_.emplace_back([this] {
+//                 for (;;) {
+//                     std::function<void()> task;
+//                     {
+//                         std::unique_lock<std::mutex> lock(queue_mutex_);
+//                         condition_.wait(lock, [this] { return stop || !tasks_.empty(); });
+//                         if (stop && tasks_.empty()) return;
+//                         task = std::move(tasks_.front());
+//                         tasks_.pop();
+//                     }
+//                     task();
+//                 }
+//             });
+//     }
+
+//     // 提交任务到池子，返回 std::future<ReturnType>
+//     template<class F, class... Args>
+//     auto enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type> {
+//         using ReturnType = typename std::result_of<F(Args...)>::type;
+//         auto task = std::make_shared< std::packaged_task<ReturnType()> >(
+//             std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+//         );
+//         std::future<ReturnType> res = task->get_future();
+//         {
+//             std::unique_lock<std::mutex> lock(queue_mutex_);
+//             if (stop) throw std::runtime_error("enqueue on stopped ThreadPool");
+//             tasks_.emplace([task]() { (*task)(); });
+//         }
+//         condition_.notify_one();
+//         return res;
+//     }
+
+//     ~ThreadPool() {
+//         {
+//             std::unique_lock<std::mutex> lock(queue_mutex_);
+//             stop = true;
+//         }
+//         condition_.notify_all();
+//         for (std::thread& worker : workers_) worker.join();
+//     }
+
+// private:
+//     std::vector<std::thread> workers_;
+//     std::queue<std::function<void()>> tasks_;
+//     std::mutex queue_mutex_;
+//     std::condition_variable condition_;
+//     bool stop;
+// };
+
+static ThreadPool pool;
 
 template<typename T>
 class FuturePipe {
@@ -80,7 +120,6 @@ public:
     template<typename F>
     auto operator|(F&& f) {
         using RET = decltype(f(std::declval<T>()));
-
         return FuturePipe<RET>(std::async(std::launch::async,
             [f, fut = std::move(currentFuture)]() mutable -> RET {
                 return f(fut.get());
@@ -95,9 +134,8 @@ private:
 
 template<typename T>
 FuturePipe<T> make_future(const T& t) {
-    return FuturePipe<T>(std::async(std::launch::async,
-        [t]() {
-            return t;
-        }
-    ));
+    return FuturePipe<T>(pool.enqueue(
+        [](const T& val) {
+            return val;
+        }, t));
 }
