c086821f7324c714a6c66edc10817b4f1350cc50
way <wayming.z@gmail.com>
2025-11-27 11:37:39 +1000

    d23

--- CPP11STL/CPP11STL/d23.h
+++ CPP11STL/CPP11STL/d23.h
@@ -11,11 +11,15 @@
 #include <queue>
 #include <future>
 #include <condition_variable>
-
+#include <tuple>
+#include <chrono>
+#include <thread>
 
 class RotateLogger {
 public:
-    RotateLogger(const std::string& fileName, size_t nbytes) : baseName(fileName), nbytes(nbytes) {
+    RotateLogger(const std::string& fileName, size_t nbytes) : baseFile(fileName), nbytes(nbytes) {
+        out = LogStream(new std::ofstream(baseFile, std::ios::out | std::ios::trunc), OfStreamDeleter());
+        if (!out->good()) throw std::runtime_error("Failed to open file " + baseFile);
         fut = std::async(&RotateLogger::logWriter, this);
     }
     ~RotateLogger() {
@@ -24,65 +28,92 @@ public:
         try {
             fut.get();
         } catch (std::exception& e) {
-            out.flush();
             std::cout << "caught: " << e.what() << std::endl;
         }
     }
-    void log(std::initializer_list<std::string> messages) {
-        if (stop) { std::cerr << "logger closing" << std::endl; return; }
+    auto log(std::initializer_list<std::string> messages) {
+        std::vector<LogFuture> futures;
+
+        if (stop) { std::cerr << "logger closing" << std::endl; return futures; }
 
         std::lock_guard<std::mutex> lock(mtx);
-        for (auto& m : messages) {
-            msgQueue.push(std::move(m));
+        for (const auto& m : messages) {
+            LogPromise p;
+            futures.push_back(p.get_future());
+            msgQueue.push(std::make_unique<LogMessage>(
+                std::make_tuple(m, std::move(p), std::chrono::steady_clock::now())));
             cv.notify_one();
         }
+
+        return futures;
     }
 
 private:
     bool logWriter() {
-        file = baseName + "." + std::to_string(fileSeq);
-        out.open(file, std::iostream::trunc);
-        if (!out.good()) {
-            throw std::runtime_error("Failed to open file " + file);
-        }
         while(true) {
-            std::string msg;
+            std::unique_ptr<LogMessage> msg;
             {
                 std::unique_lock<std::mutex> lock(mtx);
                 cv.wait(lock, [this]() { return !msgQueue.empty() or stop; });
                 if (stop and msgQueue.empty()) break;
-                msg = msgQueue.front();
+                msg = std::move(msgQueue.front());
                 msgQueue.pop();
             }
-            
-            if (out.tellp() > nbytes) rotate();
+            *out << std::get<0>(*msg) << "\n";
+            std::this_thread::sleep_for(std::chrono::milliseconds(5)); // simulate delay
+            std::get<1>(*msg).set_value(std::chrono::steady_clock::now() - std::get<2>(*msg));
+            if (out->tellp() > nbytes) rotate();
         }
+
         return true;
     }
 
     void rotate() {
-        out.flush();
-        out.close();
-        file = nextName();
-        out.open(file, std::iostream::trunc);
-        if (!out.good()) {
-            throw std::runtime_error("Failed to open file " + file);
+        if (logFiles.size() < 5) logFiles.push_back(baseFile + "." + std::to_string(logFiles.size()));
+        for (int idx = logFiles.size() - 2; idx >= 0; --idx) {
+            if (idx == 4) continue; // discard the last one
+            // std::cout << "rename " << logFiles[idx] << " to " << logFiles[idx+1] << std::endl;
+            std::rename(logFiles[idx].c_str(), logFiles[idx+1].c_str());
+        }
+
+        std::rename(baseFile.c_str(), logFiles[0].c_str());
+        // std::cout << "rename " << baseFile << " to " << logFiles[0] << std::endl;
+
+        if(out && out->is_open()) {
+            out->flush();
+            out->close();
+        }
+        out->open(baseFile, std::ios::out | std::ios::trunc);
+        if (!out->good()) {
+            throw std::runtime_error("Failed to open file " + baseFile);
         }
     }
 
-    std::string nextName() {
-        if (++fileSeq == 5) fileSeq = 0;
-        return baseName + "." + std::to_string(fileSeq);
-    }
-
 private:
-    std::string baseName;
-    std::string file;
+
+    struct OfStreamDeleter {
+        void operator()(std::ofstream* os) const {
+            if (os && os->is_open()) {
+                os->flush();
+                os->close();
+            } 
+            delete os;
+        }
+    };
+    using LogStream = std::unique_ptr<std::ofstream, OfStreamDeleter>;
+    using LogPromise = std::promise<std::chrono::steady_clock::duration>;
+    using LogFuture = std::future<std::chrono::steady_clock::duration>;
+    using LogMessage = std::tuple<
+        std::string,
+        LogPromise,
+        std::chrono::steady_clock::time_point>;
+    std::string baseFile;
     size_t nbytes;
     std::atomic<int> fileSeq = 0;
-    std::ofstream out;
+    std::vector<std::string> logFiles = {};
+    LogStream out;
 
-    std::queue<std::string> msgQueue;
+    std::queue<std::unique_ptr<LogMessage>> msgQueue = {};
     std::mutex mtx;
     std::condition_variable cv;
     std::atomic<bool> stop = false;
--- CPP11STL/tests/test_d23.cpp
+++ CPP11STL/tests/test_d23.cpp
@@ -3,22 +3,84 @@
 #include <thread>
 #include <filesystem>
 
-TEST(RotateLoggerTest, SingleUser) {
-	RotateLogger logger("d23.log", 10);
-	for (int i = 0; i < 100; ++i) {
-		logger.log({
-			"this is the 1st message",
-			"this is the 2nd message",
-			"this is the 3rd message",
-		});
+using LogFuture = std::future<std::chrono::steady_clock::duration>;
+
+class RotateLoggerTest : public ::testing::Test {
+protected:
+	void SetUp() override {
+		std::error_code ec;
+		std::cout << "RotateLoggerTest SetUp" << std::endl;
+		for (auto& file : std::filesystem::directory_iterator(".")) {
+			if (file.path().filename().string().find("d23SingleUser.log") != std::string::npos) {
+				std::cout << "delete " << file.path().filename() << std::endl;
+				std::filesystem::remove(file.path(), ec);
+			}
+		}
 	}
+};
+TEST_F(RotateLoggerTest, SingleUser) {
+	{
+		RotateLogger logger("d23SingleUser.log", 100);
+		std::vector<LogFuture> futures;
+		for (int i = 0; i < 10; ++i) {
+			auto thisFutures = logger.log({
+				"this is the 1st message",
+				"this is the 2nd message",
+				"this is the 3rd message",
+			});
+
+			std::move(thisFutures.begin(), thisFutures.end(), std::back_inserter(futures));
+		}
+
+		for (auto& f : futures) {
+			auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(f.get());
+			std::cout << "log message in " << ms.count() << "ms" << std::endl;
+		}
+	}
+
 
 	std::vector<std::string> files;
 	for (auto& file : std::filesystem::directory_iterator(".")) {
-		if (file.path().filename().string().find("d23.log") != std::string::npos) {
+		if (file.path().filename().string().find("d23SingleUser.log") != std::string::npos) {
 			files.push_back(file.path().filename().string());
 		}
 	}
 	for (auto& f: files) std::cout << f << std::endl;
-	ASSERT_EQ(files.size(), 5);
+	ASSERT_EQ(files.size(), 6);
+}
+
+
+TEST_F(RotateLoggerTest, MultipleUsers) {
+	{
+		RotateLogger logger("d23MultipleUsers.log", 100);
+		std::vector<std::thread> threads;
+		for (int i = 0; i < 10; ++i) {
+			threads.emplace_back([&logger]() {
+				for (int j = 0; j < 10; ++j) {
+					auto futures = logger.log({
+						"this is the 1st message",
+						"this is the 2nd message",
+						"this is the 3rd message",
+					});
+					for (auto& f : futures) {
+						auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(f.get());
+						std::cout << "log message in " << ms.count() << "ms" << std::endl;
+					}
+				}
+			});
+		}
+
+		for (auto& t : threads) {
+			t.join();
+		}
+	}
+
+	std::vector<std::string> files;
+	for (auto& file : std::filesystem::directory_iterator(".")) {
+		if (file.path().filename().string().find("d23MultipleUsers.log") != std::string::npos) {
+			files.push_back(file.path().filename().string());
+		}
+	}
+	for (auto& f: files) std::cout << f << std::endl;
+	ASSERT_EQ(files.size(), 6);
 }
