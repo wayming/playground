7509a5ec7cc17698e9bf10b5df3e30b3f2db3559
way <wayming.z@gmail.com>
2025-11-30 22:57:13 +1000

    input and output iterators

--- CPP11STL/CPP11STL/d25.h
+++ CPP11STL/CPP11STL/d25.h
@@ -14,3 +14,76 @@ std::vector<std::string> split(const std::string src) {
     }
     return tokens;
 }
+
+class CSVInputIterator {
+public:
+    using ElementType = std::vector<std::string>;
+
+    CSVInputIterator(std::istream& s) : is(s) {
+        ++(*this);
+    }
+
+    ElementType& operator*() {return element;}
+    ElementType* operator->() {return &element;}
+    CSVInputIterator& operator++() {
+        if (end) return *this;
+        std::string line;
+        if (!std::getline(is, line)) {
+            end = true;
+            return *this;
+        }
+
+        element = std::move(split(line));
+        if (element.size() == 0) return ++(*this);
+        return *this;
+    }
+    CSVInputIterator& operator++(int) {
+        return ++(*this);
+    }
+    bool operator==(const CSVInputIterator& other) {
+        return end && other.end;
+    }
+    bool operator!=(const CSVInputIterator& other) {
+        return !(*this == other);
+    }
+private:
+    std::istream& is;
+    ElementType element;
+    bool end = false;
+};
+
+class JSONOutputIterator {
+public:
+    JSONOutputIterator(std::ostream& s, std::vector<std::string>&& h) :os(s), headers(std::move(h)) {}
+    ~JSONOutputIterator() {}
+    JSONOutputIterator& operator=(const std::vector<std::string>& fields) {
+        if (fields.size() != headers.size()) {
+            std::stringstream error;
+            error << "Number of fields " << fields.size() << " does not match number of header " << headers.size(); 
+            throw std::runtime_error(error.str());
+        }
+        
+        if (!first) os << ", ";
+        first = false;
+
+        os << "{";
+        for (int i = 0; i < headers.size(); ++i) {
+            os << '"' << headers.at(i) << '"' << ':';
+            os << '"' << fields.at(i) << '"';
+            if (i < headers.size() - 1) {
+                os << ", ";
+            }
+        }
+        os << "}";
+
+        return *this;
+    }
+    JSONOutputIterator& operator*() {return *this;}
+    JSONOutputIterator& operator++() {return *this;}
+    JSONOutputIterator& operator++(int) {return *this;}
+
+private:
+    std::ostream& os;
+    std::vector<std::string> headers;
+    bool first = true;
+};
--- CPP11STL/tests/test_d25.cpp
+++ CPP11STL/tests/test_d25.cpp
@@ -7,3 +7,26 @@ TEST(CVSToJsonConverterTest, Sanity) {
 	ASSERT_EQ(tokens.size(), 4);
 	ASSERT_EQ(tokens, expected);
 }
+
+TEST(CVSToJsonConverterTest, CSVInput) {
+	const std::string csvText =
+		"aa,bb,cc\n"
+		"dd,ee,ff\n";
+	std::stringstream ss(csvText);
+	CSVInputIterator iter(ss);
+	std::vector<std::string> expectedLine1 = {"aa", "bb", "cc"};
+	std::vector<std::string> expectedLine2 = {"dd", "ee", "ff"};
+	ASSERT_EQ(*iter, expectedLine1);
+	ASSERT_EQ(*(++iter), expectedLine2);
+}
+
+TEST(CVSToJsonConverterTest, JSONOutput) {
+	std::stringstream ss;
+	JSONOutputIterator iter(ss, {"field1", "field2", "field3"});
+	iter = {"aa", "bb", "cc"};
+	std::cout << ss.str() << std::endl;
+	ASSERT_EQ(ss.str(), R"RAW({"field1":"aa", "field2":"bb", "field3":"cc"})RAW");
+	iter = {"dd", "ee", "ff"};
+	std::cout << ss.str() << std::endl;
+	ASSERT_EQ(ss.str(), R"RAW({"field1":"aa", "field2":"bb", "field3":"cc"}, {"field1":"dd", "field2":"ee", "field3":"ff"})RAW");
+}
