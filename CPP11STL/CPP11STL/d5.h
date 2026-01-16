1414d824ebe39c4bc16f63708579614a6e7c8513
way <wayming.z@gmail.com>
2026-01-16 22:47:43 +1000

    d5

--- CPP11STL/CPP11STL/d5.h
+++ CPP11STL/CPP11STL/d5.h
@@ -6,41 +6,48 @@
 #include <iostream>
 #include <atomic>
 #include <thread>
-void run(int n) {
-	std::default_random_engine gen;
-	std::uniform_int_distribution<int> dist(100, 500);
-	std::vector<int> randomDelay(n);
-	std::generate(randomDelay.begin(), randomDelay.end(), [&gen, &dist]() {return dist(gen); });
-	std::vector<std::future<int>> futures;
-	futures.reserve(randomDelay.size());
-	std::mutex mtx;
-	std::condition_variable start;
-	std::atomic<int> readyThreads = 0;
-	for (auto delay : randomDelay) {
-		futures.emplace_back(std::async([&start, &mtx, &readyThreads](int n) {
-			{
-				std::unique_lock<std::mutex> lock(mtx);
-				readyThreads++;
-				std::cout << readyThreads << std::endl;
-				start.wait(lock);
-			}
-			std::chrono::system_clock::time_point begin = std::chrono::system_clock::now();
-			std::this_thread::sleep_for(std::chrono::milliseconds(n));
-			std::chrono::system_clock::duration defer = std::chrono::system_clock::now() - begin;
-			std::cout << "delay " << defer.count() << "ms" << std::endl;
-			return n * n;
-			}, delay));
-	}
+#include <functional>
 
-	while (true)
-	{
-		std::cout << readyThreads << std::endl;
-		if (readyThreads == n) break;
-		std::this_thread::sleep_for(std::chrono::milliseconds(5));
+class RandomeTaskScheduler {
+public:
+	template<typename f>
+	void run(int n, std::function<int(int)> f) {
+		for(int i = 0; i < n; ++i) {
+			std::packaged_task<int(int)> t(f);
+			futures.push_back(t.get_future());
+			threads.emplace_back(std::move(t), dist(gen));
+		}
 	}
-	start.notify_all();
-	std::chrono::system_clock::time_point begin = std::chrono::system_clock::now();
-	for (auto& f : futures) std::cout << f.get() << std::endl;
-	std::chrono::system_clock::duration defer = std::chrono::system_clock::now() - begin;
-	std::cout << "total delay " << defer.count() << "ms" << std::endl;
+	auto results() {
+		std::vector<int> res;
+
+		for(auto& t : threads) {
+			t.join();
+		}
+		threads.clear();
+
+		for(auto& f : futures) {
+			res.push_back(f.get());
+		}
+		futures.clear();
+		return res;
+	}
+private:
+	std::vector<std::future<int>> futures;
+	std::vector<std::thread> threads;
+	std::mt19937 gen{std::random_device{}()};
+	std::uniform_int_distribution<int> dist{0, 99};
+};
+
+auto runSqureTasks(int n) {
+	RandomeTaskScheduler sche;
+	sche.run(n, [](int x) {
+		auto now = std::chrono::steady_clock::now();
+		std::this_thread::sleep_for(std::chrono::milliseconds(rand()%1000));
+		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - now);
+		std::cout << "Calculate " << x << " took " << duration.count() << "ms" << std::endl;
+		return x * x;
+	});
+
+	return sche.results();
 }
--- CPP11STL/tests/test_d5.cpp
+++ CPP11STL/tests/test_d5.cpp
@@ -2,5 +2,10 @@
 #include "../CPP11STL/d5.h"
 
 TEST(AsyncTests, Sanity) {
-	run(10);
+	auto results = runSqureTasks(5);
+	ASSERT_EQ(results.size(), 5);
+	for (auto& x : results) {
+		std::cout << x << ",";
+	}
+	std::cout << std::endl;
 }
