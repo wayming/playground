0f4d8a4b9a9a240492b90baefa0b08cdeca98f31
way <wayming.z@gmail.com>
2026-01-26 23:41:42 +1000

    strings

--- CPP11STL/CPP11STL/d12.h
+++ CPP11STL/CPP11STL/d12.h
@@ -5,144 +5,67 @@
 #include <sstream>
 #include <deque>
 #include <tuple>
-struct CommandParser {
-	using Func = std::function<std::any(const std::vector<std::any>&)>;
+#include <variant>
+#include <regex>
 
+std::vector<std::string> split(const std::string& src) {
+	std::vector<std::string> tokens;
+	size_t start = 0;
+	while(start != std::string::npos) {
+		auto tokenBegin = src.find_first_not_of(' ', start);
+		if (tokenBegin == std::string::npos ) break; // end of string
+		auto tokenEnd = src.find_first_of(' ', tokenBegin);
+		tokens.emplace_back(src.substr(tokenBegin, tokenEnd-tokenBegin));
+		start = tokenEnd;
+	}
+	return tokens;
+}
+
+
+
+std::vector<std::string> splitRe(const std::string& src) {
+	std::regex e("\\w+");
+	std::sregex_token_iterator begin(src.begin(), src.end(), e);
+	std::sregex_token_iterator end;
+	return std::vector<std::string>(begin, end);
+}
+class CommandParser {
+	using ValueType = std::variant<long, double, std::string>;
+	using OpFuncType = std::function<void(const std::vector<ValueType>&)>;
+	std::unordered_map<std::string, OpFuncType> commandReg;
+public:
 	CommandParser() {
-		regFunc("ADD", [](const std::vector<std::any>& v) {
-			if (v.size() != 2) {
-				throw std::runtime_error("Expecting two parameters for ADD operation");
+		regFunc("ADD", [](const std::vector<ValueType>& params) -> ValueType {
+			if(params.size() != 2) throw std::runtime_error("Invalid number of parameters for ADD op");
+			if (std::holds_alternative<std::string>(params[0]) &&
+				std::holds_alternative<std::string>(params[1])) {
+					return std::get<std::string>(params[0]) + std::get<std::string>(params[1]);
 			}
-			const auto& param1Type = v[0].type();
-			const auto& param2Type = v[1].type();
-			if (param1Type != typeid(std::string) || param2Type != typeid(std::string)) {
-				throw std::runtime_error("Invalid parameter type.");
+			else if (std::holds_alternative<long>(params[0]) &&
+				std::holds_alternative<long>(params[1])) {
+					return std::get<long>(params[0]) + std::get<long>(params[1]);
 			}
-			try {
-				auto op1 = std::stol(std::any_cast<std::string>(v[0]));
-				auto op2 = std::stol(std::any_cast<std::string>(v[1]));
-				return op1 + op2;
-			} catch (std::exception& e) {
-				std::cerr << "Failed to cast any value to its type " << param1Type.name() << std::endl;
-				throw e;
-			}
-		});
-
-		regFunc("MULT", [](const std::vector<std::any>& v) {
-			if (v.size() != 2) {
-				throw std::runtime_error("Expecting two parameters for ADD operation");
-			}
-			const auto& param1Type = v[0].type();
-			const auto& param2Type = v[1].type();
-			if (param1Type != typeid(std::string) || param2Type != typeid(std::string)) {
-				throw std::runtime_error("Invalid parameter type.");
-			}
-			try {
-				auto op1 = std::stol(std::any_cast<std::string>(v[0]));
-				auto op2 = std::stol(std::any_cast<std::string>(v[1]));
-				return op1 * op2;
-			}
-			catch (std::exception& e) {
-				std::cerr << "Failed to cast any value to its type " << param1Type.name() << std::endl;
-				throw e;
-			}
-		});
-		regFunc("ECHO", [](const std::vector<std::any>& v) {
-			if (v.size() != 1) {
-				throw std::runtime_error("Expecting two parameters for ADD operation");
-			}
-			const auto& param1Type = v[0].type();
-			if (param1Type != typeid(std::string)) {
-				throw std::runtime_error("Invalid parameter type.");
-			}
-			try {
-				return std::any_cast<std::string>(v[0]);
-			}
-			catch (std::exception& e) {
-				std::cerr << "Failed to cast any value to its type " << param1Type.name() << std::endl;
-				throw e;
+			else if (std::holds_alternative<double>(params[0]) &&
+				std::holds_alternative<double>(params[1])) {
+					return std::get<double>(params[0]) + std::get<double>(params[1]);
+			} else {
+				throw std::runtime_error("Invalid types of operands.");
 			}
 		});
 	}
-	void addCommand(const std::string& cmd) {
-		std::regex pattern(R"(\s*(\w+))");
-		auto patternIter = std::sregex_iterator(cmd.begin(), cmd.end(), pattern);
-		std::sregex_iterator patternEnd;
-		std::vector<std::string> tokens;
-		while(patternIter != patternEnd) {
-			tokens.push_back(patternIter->str());
-			++patternIter;
-		}
 
-		if (tokens.empty()) {
-			throw std::runtime_error("Failed to parse command string " + cmd);
-		}
-		const std::string& key = tokens[0];
-		std::vector<std::string> params;
-		params.reserve(tokens.size() - 1);
-		std::move(tokens.begin() + 1, tokens.end(), std::back_inserter(params));
-		commandsQueue.emplace_back(key, std::move(params));
+	void addCommand(const std::string& cmd) {
+
 	}
 
 	void dump() {
-		for (auto& cmd : commandsQueue) {
-			std::cout << '"' << std::get<0>(cmd) << '"' << ": [";
-			auto& v = std::get<1>(cmd);
-			if (!v.empty()) {
-				auto iter = v.begin();
-				std::cout << *iter;
-				++iter;
-				while (iter != v.end()) {
-					std::cout << ", " << *iter;
-					++iter;
-				}
-			}
-
-			std::cout << "]" << std::endl;
-		}
-
 	}
 
 	void eval() {
-
-		for (auto& cmd : commandsQueue) {
-			std::string op;
-			std::vector<std::string> params;
-			std::tie(op, params) = cmd;
-			if (op.empty()) {
-				throw std::runtime_error("No handler for operation " + op);
-			}
-			std::vector<std::any> anyParams;
-			std::copy(params.begin(), params.end(), std::back_inserter(anyParams));
-			std::cout << anyToString(funcsMap[op](anyParams)) << std::endl;
-		}
 	}
 
-	std::string anyToString(const std::any& val) {
-		if (!val.has_value()) {
-			throw std::runtime_error("No value found.");
-		}
-
-		const auto& valType = val.type();
-		try {
-			if (valType == typeid(int)) return std::to_string(std::any_cast<int>(val));
-			if (valType == typeid(long)) return std::to_string(std::any_cast<long>(val));
-			if (valType == typeid(std::string)) return std::any_cast<std::string>(val);
-		}
-		catch (std::exception& e) {
-			std::cerr << "Failed to cast any value to its type " << valType.name() << std::endl;
-			throw e;
-		}
-		return "NULL";
-
+	void regFunc(const std::string& op, OpFuncType f) {
+		commandReg[op] = f;
 	}
-	void regFunc(const std::string& op, Func f) {
-		funcsMap[op] = f;
-	}
-
-	std::unordered_map<std::string, Func> funcsMap;
-
-	std::deque<std::tuple<std::string, std::vector<std::string>>> commandsQueue; // OP => Params
-
 };
 
--- CPP11STL/tests/test_d12.cpp
+++ CPP11STL/tests/test_d12.cpp
@@ -1,6 +1,29 @@
 #include <gtest/gtest.h>
 #include "../CPP11STL/d12.h"
 
+TEST(CommandRunnerTest, Split) {
+	auto v1 = split("ADD a b");
+	ASSERT_EQ(v1.size(), 3);
+	ASSERT_STREQ(v1.at(0).c_str(), "ADD");
+	ASSERT_STREQ(v1.at(1).c_str(), "a");
+	ASSERT_STREQ(v1.at(2).c_str(), "b");
+
+	v1 = split("  ADD  a ");
+	ASSERT_EQ(v1.size(), 2);
+	ASSERT_STREQ(v1.at(0).c_str(), "ADD");
+	ASSERT_STREQ(v1.at(1).c_str(), "a");
+
+	v1 = splitRe("ADD a b");
+	ASSERT_EQ(v1.size(), 3);
+	ASSERT_STREQ(v1.at(0).c_str(), "ADD");
+	ASSERT_STREQ(v1.at(1).c_str(), "a");
+	ASSERT_STREQ(v1.at(2).c_str(), "b");
+
+	v1 = splitRe("  ADD  a ");
+	ASSERT_EQ(v1.size(), 2);
+	ASSERT_STREQ(v1.at(0).c_str(), "ADD");
+	ASSERT_STREQ(v1.at(1).c_str(), "a");
+}
 TEST(CommandRunnerTest, Sanity) {
 	CommandParser parser;
 	parser.addCommand("ADD 3 5");
