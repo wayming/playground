7ca5678ae4095075ac57c57576ca5a23806b11a4
way <wayming.z@gmail.com>
2026-02-06 17:18:39 +1000

    t8

--- pybook/pybook/t8.py
+++ pybook/pybook/t8.py
@@ -1,7 +1,9 @@
+import asyncio
+import multiprocessing
 import threading
 
 
-def counter_mt(nthreads, niterations) -> int:
+def counter_mt(nParallel, nIterations) -> int:
     data = {"count": 0}
     lock = threading.Lock()
 
@@ -11,8 +13,8 @@ def counter_mt(nthreads, niterations) -> int:
                 data["count"] += 1
 
     threads = []
-    for _ in range(nthreads):
-        threads.append(threading.Thread(target=compute, args=[niterations]))
+    for _ in range(nParallel):
+        threads.append(threading.Thread(target=compute, args=[nIterations]))
 
     for t in threads:
         t.start()
@@ -21,3 +23,43 @@ def counter_mt(nthreads, niterations) -> int:
         t.join()
 
     return data["count"]
+
+
+def counter_mp(nParallel, nIterations) -> int:
+    data = multiprocessing.Value("i", 0, lock=True)
+
+    def compute(v: multiprocessing.Value, n: int):
+        for _ in range(n):
+            with v.get_lock():
+                v.value += 1
+
+    processes = []
+    for _ in range(nParallel):
+        processes.append(
+            multiprocessing.Process(target=compute, args=[data, nIterations])
+        )
+
+    for p in processes:
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    return data.value
+
+
+def counter_async(nParallel, nIteration) -> int:
+    value = {"count": 0}
+
+    async def acounter_async(p, i) -> int:
+        async def acompute(n):
+            for _ in range(n):
+                value["count"] += 1
+                if _ % 1000 == 0:
+                    await asyncio.sleep(0)
+
+        tasks = [acompute(i) for _ in range(p)]
+        await asyncio.gather(*tasks)
+
+    asyncio.run(acounter_async(nParallel, nIteration))
+    return value["count"]
--- pybook/tests/test_t8.py
+++ pybook/tests/test_t8.py
@@ -6,8 +6,18 @@ import pybook.t8 as t
 sys.setswitchinterval(0.000001)
 
 
-def test_counter_mt():
+def test_counter_parallel():
     begin = time.perf_counter()
-    print(t.counter_mt(20, 100000000))
+    print(t.counter_mt(20, 100000))
     end = time.perf_counter()
-    print(f"total elapsed {end - begin} seconds")
+    print(f"mt total elapsed {end - begin} seconds")
+
+    begin = time.perf_counter()
+    print(t.counter_mp(20, 100000))
+    end = time.perf_counter()
+    print(f"mp total elapsed {end - begin} seconds")
+
+    begin = time.perf_counter()
+    print(t.counter_async(20, 100000))
+    end = time.perf_counter()
+    print(f"async total elapsed {end - begin} seconds")
