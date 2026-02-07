75956edc3623ea1e9967e07793987b9ec013353c
way <wayming.z@gmail.com>
2026-02-07 23:04:24 +1000

    inorder

--- pybook/pybook/t15.py
+++ pybook/pybook/t15.py
@@ -1,5 +1,5 @@
 # Assumes complete binary tree
-def inorder(nodes: list[int]):
+def preorder(nodes: list[int]):
     indexStack = []
     total = len(nodes)
     results = []
@@ -12,4 +12,21 @@ def inorder(nodes: list[int]):
             indexStack.append(2 * curr + 2)
         if 2 * curr + 1 <= total - 1:
             indexStack.append(2 * curr + 1)
+    return results
+
+
+# Assumes complete binary tree
+def inorder(nodes: list[int]):
+    indexStack = []
+    total = len(nodes)
+    results = []
+    curr = 0
+    while len(indexStack) > 0 or curr < total:
+        while curr < total:
+            indexStack.append(curr)
+            curr = 2 * curr + 1
+        curr = indexStack.pop()
+        results.append(nodes[curr])
+        curr = 2 * curr + 2
+
     return results
--- pybook/tests/test_t15.py
+++ pybook/tests/test_t15.py
@@ -1,6 +1,11 @@
 import pybook.t15 as t
 
 
+def test_preorder():
+    nodes = [x for x in range(1, 10)]
+    assert t.preorder(nodes) == [1, 2, 4, 8, 9, 5, 3, 6, 7]
+
+
 def test_inorder():
     nodes = [x for x in range(1, 10)]
-    assert t.inorder(nodes) == [1, 2, 4, 8, 9, 5, 3, 6, 7]
+    assert t.inorder(nodes) == [8, 4, 9, 2, 5, 1, 6, 3, 7]
