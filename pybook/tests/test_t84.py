d3e616d64c75b4bea0e4cd5784ac62bcaae29902
way <wayming.z@gmail.com>
2026-02-16 22:53:15 +1000

    84

--- pybook/tests/test_t84.py
+++ pybook/tests/test_t84.py
@@ -1,3 +1,5 @@
+import pytest
+
 import pybook.t84 as t
 
 
@@ -5,5 +7,6 @@ def test_t84_short_url():
     print(t.short_url("http://www.sohu.com", 8))
     print(t.short_url("http://www.sohu.com", 1))
     print(t.short_url("http://www.sohu.com", 16))
-    print(t.short_url("http://www.sohu.com", 160))
     assert len(t.short_url("http://www.sohu.com", 8)) == 8
+    with pytest.raises(ValueError):
+        assert print(t.short_url("http://www.sohu.com", 160))
